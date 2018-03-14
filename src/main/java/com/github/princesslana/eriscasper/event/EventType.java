ece7af88643f7911850b289a54b5ca7b98ef66de
ianagbip1oti <ianagbip1oti@gmail.com>
2018-03-14 07:28:52 +0000

    Updated docs

--- src/main/java/com/github/princesslana/eriscasper/event/EventType.java
+++ src/main/java/com/github/princesslana/eriscasper/event/EventType.java
@@ -4,6 +4,19 @@ import com.github.princesslana.eriscasper.data.GuildCreateData;
 import com.github.princesslana.eriscasper.data.ReadyData;
 import java.util.function.Function;
 
+/**
+ * Possible event types that we can receive from Discord.
+ *
+ * <p>Each EventType is associated with an {@link EventFactory} so that it is known how to construct
+ * an {@link Event} from a {@link com.github.princesslana.eriscasper.gateway.Payload}.
+ *
+ * <p>It is tempting to try and merge EventType and {@link EventFactory} into a single class, but we
+ * can not match the dataClass and factory types using generics within the EventType enum.
+ *
+ * @see <a
+ *     href="https://discordapp.com/developers/docs/topics/gateway#commands-and-events-gateway-events">
+ *     https://discordapp.com/developers/docs/topics/gateway#commands-and-events-gateway-events</a>
+ */
 public enum EventType {
   GUILD_CREATE(GuildCreateData.class, ImmutableGuildCreate::of),
   READY(ReadyData.class, ImmutableReady::of);
