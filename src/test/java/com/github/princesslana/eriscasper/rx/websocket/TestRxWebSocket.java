0db6102b4f832b31ce9bf800fd9bfdf9245e316d
ianagbip1oti <ianagbip1oti@gmail.com>
2018-03-16 03:03:40 +0000

    Add RxWebSocket#close tests

--- src/test/java/com/github/princesslana/eriscasper/rx/websocket/TestRxWebSocket.java
+++ src/test/java/com/github/princesslana/eriscasper/rx/websocket/TestRxWebSocket.java
@@ -1,8 +1,8 @@
 package com.github.princesslana.eriscasper.rx.websocket;
 
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.mock;
 import static org.mockito.BDDMockito.notNull;
+import static org.mockito.BDDMockito.then;
 
 import io.reactivex.subscribers.TestSubscriber;
 import okhttp3.OkHttpClient;
@@ -11,19 +11,28 @@ import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
 import org.assertj.core.api.Assertions;
 import org.mockito.ArgumentCaptor;
-import org.testng.annotations.BeforeTest;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestRxWebSocket {
 
-  private OkHttpClient mockClient;
+  private static final String TEST_WS_URL = "wss://test.url/";
+
+  @Mock private OkHttpClient mockClient;
+
+  @Mock private WebSocket mockWebSocket;
 
   private RxWebSocket subject;
 
-  @BeforeTest
-  public void setupSubject() {
-    mockClient = mock(OkHttpClient.class);
+  @BeforeMethod
+  public void mockito() {
+    MockitoAnnotations.initMocks(this);
+  }
 
+  @BeforeMethod
+  public void setupSubject() {
     subject = new RxWebSocket(mockClient);
   }
 
@@ -33,25 +42,38 @@ public class TestRxWebSocket {
 
     given(mockClient.newWebSocket(request.capture(), notNull())).willReturn(null);
 
-    subject.connect("wss://test.url").subscribe();
+    subject.connect(TEST_WS_URL).subscribe();
 
     Assertions.assertThat(request.getValue().url().toString()).isEqualTo("https://test.url/");
   }
 
   @Test
   public void connect_whenStringMessage_emitsStringMessageEvent() {
-    WebSocket mockWebSocket = mock(WebSocket.class);
-
     ArgumentCaptor<WebSocketListener> wsl = ArgumentCaptor.forClass(WebSocketListener.class);
 
     TestSubscriber<RxWebSocketEvent> subscriber = new TestSubscriber<>();
 
     given(mockClient.newWebSocket(notNull(), wsl.capture())).willReturn(mockWebSocket);
 
-    subject.connect("wss://test.url").subscribe(subscriber);
+    subject.connect(TEST_WS_URL).subscribe(subscriber);
 
     wsl.getValue().onMessage(mockWebSocket, "test message");
 
     subscriber.assertValuesOnly(StringMessageTuple.of(mockWebSocket, "test message"));
+  }
+
+  @Test
+  public void close_whenBeforeConnect_doesNotError() {
+    Assertions.assertThatCode(() -> subject.close()).doesNotThrowAnyException();
+  }
+
+  @Test
+  public void close_whenAfterSubscribe_closesWebSocket() {
+    given(mockClient.newWebSocket(notNull(), notNull())).willReturn(mockWebSocket);
+
+    subject.connect(TEST_WS_URL).subscribe();
+    subject.close();
+
+    then(mockWebSocket).should().close(1000, "Bye.");
   }
 }
