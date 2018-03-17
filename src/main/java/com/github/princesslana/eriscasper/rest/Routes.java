0a33b2b5744e59482930f195ec0cd68132bae89d
ianagbip1oti <ianagbip1oti@gmail.com>
2018-03-17 23:33:00 +0000

    Refactor HTTP requset sending

--- src/main/java/com/github/princesslana/eriscasper/rest/Routes.java
+++ src/main/java/com/github/princesslana/eriscasper/rest/Routes.java
@@ -2,7 +2,13 @@ package com.github.princesslana.eriscasper.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.princesslana.eriscasper.BotToken;
+import com.github.princesslana.eriscasper.rx.Maybes;
+import io.reactivex.Observable;
 import io.reactivex.Single;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import java.util.Optional;
+import java.util.concurrent.Callable;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -15,6 +21,8 @@ public class Routes {
 
   private static final Logger LOG = LoggerFactory.getLogger(Routes.class);
 
+  private static final MediaType MEDIA_TYPE_JSON = MediaType.parse("application/json");
+
   private final BotToken token;
 
   private final OkHttpClient client;
@@ -26,43 +34,55 @@ public class Routes {
     this.jackson = jackson;
   }
 
-  public <Rs> Single<Rs> execute(Route<Void, Rs> route) {
+  public <O> Single<O> execute(Route<Void, O> route) {
     return execute(route, null);
   }
 
-  public <Rq, Rs> Single<Rs> execute(Route<Rq, Rs> route, Rq data) {
-    return Single.fromCallable(
-            () -> {
-              LOG.debug("Executing: {}...", route);
+  public <I, O> Single<O> execute(Route<I, O> route, I data) {
+    Function<Optional<RequestBody>, Request> buildRequest =
+        body ->
+            new Request.Builder()
+                .method(route.getMethod().get(), body.orElse(null))
+                .url(route.getUrl())
+                .header("Authorization", "Bot " + token.unwrap())
+                .build();
 
-              RequestBody body =
-                  data == null
-                      ? null
-                      : RequestBody.create(
-                          MediaType.parse("application/json"), jackson.writeValueAsString(data));
+    return Maybes.fromNullable(data)
+        .map(d -> RequestBody.create(MEDIA_TYPE_JSON, jackson.writeValueAsString(data)))
+        .map(Optional::of)
+        .toSingle(Optional.empty())
+        .map(buildRequest)
+        .flatMap(rq -> executeRequest(route, rq));
+  }
 
-              Request rq =
-                  new Request.Builder()
-                      .method(route.getMethod().get(), body)
-                      .url(route.getUrl())
-                      .header("Authorization", "Bot " + token.unwrap())
-                      .build();
+  private <O> Single<O> executeRequest(Route<?, O> route, Request rq) {
+    Callable<Response> execute =
+        () -> {
+          LOG.debug("Executing: {}...", route);
+          return client.newCall(rq).execute();
+        };
 
-              try (Response rs = client.newCall(rq).execute()) {
-                String rsBody = rs.body().string();
+    Consumer<Response> close =
+        r -> {
+          r.close();
+          LOG.debug("Closed: {}.", r);
+        };
 
-                LOG.debug("Response: {}", rs);
-                LOG.debug("Headers: {}", rs.headers());
-                LOG.debug("Body: {}", rsBody);
-
-                if (!rs.isSuccessful()) {
-                  throw new IllegalStateException(String.format("Request failed: %s", rs));
-                }
-
-                return jackson.readValue(rsBody, route.getResponseClass());
-              }
-            })
-        .doOnSuccess(r -> LOG.debug("Done: {} -> {}.", route, r))
-        .doOnError(e -> LOG.warn("Error: {} - {}.", route, e));
+    // Single#using does not work here, as it performs the close operation immediately upon emitting
+    // the response, because once it's received the response it knows it can receive no more.
+    //
+    // Observable#using closes the response at a more appropriate time. My feeling is that this is
+    // wrong - we're tricking rxjava into thinking there is something coming next so as to delay the
+    // close.
+    return Observable.using(execute, Observable::just, close)
+        .doOnNext(r -> LOG.debug("Done: {} -> {}.", route, r))
+        .flatMapSingle(
+            r ->
+                r.isSuccessful()
+                    ? Single.just(r)
+                    : Single.error(new IllegalStateException("Unexpected response")))
+        .doOnError(e -> LOG.warn("Error: {} - {}.", route, e))
+        .map(rs -> jackson.readValue(rs.body().byteStream(), route.getResponseClass()))
+        .firstOrError();
   }
 }
--- src/main/java/com/github/princesslana/eriscasper/rx/Maybes.java
+++ src/main/java/com/github/princesslana/eriscasper/rx/Maybes.java
@@ -6,6 +6,10 @@ import java.util.Optional;
 public class Maybes {
   private Maybes() {}
 
+  public static <T> Maybe<T> fromNullable(T v) {
+    return v == null ? Maybe.empty() : Maybe.just(v);
+  }
+
   public static <T> Maybe<T> fromOptional(Optional<T> op) {
     return op.map(Maybe::just).orElse(Maybe.empty());
   }
