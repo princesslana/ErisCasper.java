c1e51e598f797328b6c2f81505cab347a94f1af8
ianagbip1oti <ianagbip1oti@gmail.com>
2018-03-17 22:01:57 +0000

    Add test for loading info

---
+++ src/test/java/com/github/princesslana/eriscasper/TestErisCasperInfo.java
@@ -0,0 +1,12 @@
+package com.github.princesslana.eriscasper;
+
+import org.assertj.core.api.Assertions;
+import org.testng.annotations.Test;
+
+public class TestErisCasperInfo {
+
+  @Test
+  public void load_shouldNotThrow() {
+    Assertions.assertThatCode(() -> ErisCasperInfo.load()).doesNotThrowAnyException();
+  }
+}
