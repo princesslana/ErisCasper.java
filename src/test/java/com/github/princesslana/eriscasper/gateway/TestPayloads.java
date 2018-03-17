4e694848a04f2389a61ed321803089d7bfbb8f63
ianagbip1oti <ianagbip1oti@gmail.com>
2018-03-17 01:11:25 +0000

    Add Payloads#identify test

--- src/test/java/com/github/princesslana/eriscasper/gateway/TestPayloads.java
+++ src/test/java/com/github/princesslana/eriscasper/gateway/TestPayloads.java
@@ -1,15 +1,33 @@
 package com.github.princesslana.eriscasper.gateway;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.princesslana.eriscasper.BotToken;
 import com.github.princesslana.eriscasper.gateway.Payloads.Heartbeat;
 import com.github.princesslana.eriscasper.util.Jackson;
 import java.io.IOException;
 import org.assertj.core.api.Assertions;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestPayloads {
 
   private final ObjectMapper jackson = Jackson.newObjectMapper();
+
+  private Payloads subject;
+
+  @BeforeMethod
+  public void subject() {
+    subject = new Payloads(jackson);
+  }
+
+  @Test
+  public void identify_whenBotToken_shouldHaveIdentifyOpCode() {
+    Payload p = subject.identify(BotToken.of("TEST_TOKEN"));
+
+    Assertions.assertThat(p.op()).isEqualTo(OpCode.IDENTIFY);
+    Assertions.assertThat(p.d()).isPresent();
+    Assertions.assertThat(p.d().get().get("token").asText()).isEqualTo("TEST_TOKEN");
+  }
 
   @Test
   public void heartbeat_whenValidPayload_shouldDeserialize() throws IOException {
