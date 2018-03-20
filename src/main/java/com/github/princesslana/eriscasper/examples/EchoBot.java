520a987069f4b5b8c50a4192e2f5dacd089f22a2
casper-h <caspertt2009@gmail.com>
2018-03-19 22:56:22 -0700

    Fix Format

--- src/main/java/com/github/princesslana/eriscasper/examples/EchoBot.java
+++ src/main/java/com/github/princesslana/eriscasper/examples/EchoBot.java
@@ -5,33 +5,36 @@ import com.github.princesslana.eriscasper.event.Event;
 import com.github.princesslana.eriscasper.event.Events;
 import com.github.princesslana.eriscasper.rest.ImmutableSendMessageRequest;
 import com.github.princesslana.eriscasper.rest.RouteCatalog;
-import io.reactivex.Completable;
 
 public class EchoBot {
-    public static void main(String args[]) {
-        ErisCasper.create()
-            .run(
-                ctx ->
-                    ctx.getEvents()
+  public static void main(String args[]) {
+    ErisCasper.create()
+        .run(
+            ctx ->
+                ctx.getEvents()
 
-                        //Same type as PingBot
-                        .ofType(Events.MessageCreate.class)
-                        .map(Event::getData)
+                    // Same type as PingBot in examples
+                    .ofType(Events.MessageCreate.class)
+                    .map(Event::getData)
 
-                        //Need to check for bot's own message
-                        .filter(d -> !d.getAuthor().isBot())
-                        .filter(d -> d.getContent().startsWith("+echo"))
+                    // Need to check for bot's own message
+                    .filter(d -> !d.getAuthor().isBot())
+                    .filter(d -> d.getContent().startsWith("+echo"))
+                    .flatMapCompletable(
+                        d -> {
+                          String replyMessage = d.getContent().replaceFirst("\\+echo", "");
 
-                        .flatMapCompletable( d -> {
-                            String replyMessage = d.getContent().replaceFirst("\\+echo", "");
+                          // Empty Arguments
+                          if (replyMessage.trim().isEmpty()) {
+                            replyMessage = "This command requires 1 argument";
+                          }
 
-                            //Empty Arguments
-                            if (replyMessage.trim().isEmpty()) {
-                                replyMessage = "This command requires 1 argument";
-                            }
-
-                            return ctx.execute(RouteCatalog.createMessage(d.getChannelId()),
-                                    ImmutableSendMessageRequest.builder().content(replyMessage).build()).toCompletable();
+                          return ctx.execute(
+                                  RouteCatalog.createMessage(d.getChannelId()),
+                                  ImmutableSendMessageRequest.builder()
+                                      .content(replyMessage)
+                                      .build())
+                              .toCompletable();
                         }));
-    }
+  }
 }
