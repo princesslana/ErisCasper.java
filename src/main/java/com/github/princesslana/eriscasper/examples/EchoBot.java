5b101a083bc57cd5d697cf20dc6d1fff222020c8
casper-h <caspertt2009@gmail.com>
2018-03-19 22:33:03 -0700

    Working EchoBot

---
+++ src/main/java/com/github/princesslana/eriscasper/examples/EchoBot.java
@@ -0,0 +1,32 @@
+package com.github.princesslana.eriscasper.examples;
+
+import com.github.princesslana.eriscasper.ErisCasper;
+import com.github.princesslana.eriscasper.event.Event;
+import com.github.princesslana.eriscasper.event.Events;
+import com.github.princesslana.eriscasper.rest.ImmutableSendMessageRequest;
+import com.github.princesslana.eriscasper.rest.RouteCatalog;
+import io.reactivex.Completable;
+
+public class EchoBot {
+    public static void main(String args[]) {
+        ErisCasper.create()
+            .run(
+                ctx ->
+                    ctx.getEvents()
+                        .ofType(Events.MessageCreate.class)
+                        .map(Event::getData)
+                        .filter(d -> !d.getAuthor().isBot())
+                        .filter(d -> d.getContent().startsWith("+echo"))
+
+                        .flatMapCompletable( d -> {
+                            String replyMessage = d.getContent().replaceFirst("\\+echo", "");
+
+                            if (replyMessage.trim().isEmpty()) {
+                                replyMessage = "This command requires 1 argument";
+                            }
+
+                            return ctx.execute(RouteCatalog.createMessage(d.getChannelId()),
+                                    ImmutableSendMessageRequest.builder().content(replyMessage).build()).toCompletable();
+                        }));
+    }
+}
