5e20f1bd82553d0762a3a0ac30a6477284b4c835
ianagbip1oti <ianagbip1oti@gmail.com>
2018-03-22 02:34:48 +0000

    Flesh out User data

--- src/test/java/com/github/princesslana/eriscasper/data/TestTypingStartData.java
+++ src/test/java/com/github/princesslana/eriscasper/data/TestTypingStartData.java
@@ -11,12 +11,14 @@ public class TestTypingStartData {
   @Test
   public void deserialize_whenValidPayload_shouldDeserialize() throws IOException {
     String payload =
-        "{\"user_id\":\"215210079148834816\",\"timestamp\":1521450931,\"channel_id\":\"424363501012779009\"}";
+        "{\"user_id\":\"215210079148834816\",\"timestamp\":1521450931,"
+            + "\"channel_id\":\"424363501012779009\"}";
 
     TypingStartData d = Jackson.newObjectMapper().readValue(payload, TypingStartData.class);
 
-    Assertions.assertThat(d.getUserId()).isEqualTo(UserId.of("215210079148834816"));
-    Assertions.assertThat(d.getChannelId()).isEqualTo(ChannelId.of("424363501012779009"));
-    Assertions.assertThat(d.getTimestamp()).isEqualTo(Instant.ofEpochSecond(1521450931));
+    Assertions.assertThat(d)
+        .hasFieldOrPropertyWithValue("userId", UserId.of("215210079148834816"))
+        .hasFieldOrPropertyWithValue("channelId", ChannelId.of("424363501012779009"))
+        .hasFieldOrPropertyWithValue("timestamp", Instant.ofEpochSecond(1521450931));
   }
 }
