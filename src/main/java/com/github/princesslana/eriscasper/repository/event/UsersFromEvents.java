93e6e84ddcfed57c0ee143af9517e1220acf9b5e
CoreyShupe <coreyshupe101@gmail.com>
2018-05-21 01:47:25 -0400

    Added a guild cache into the repository.

--- src/main/java/com/github/princesslana/eriscasper/repository/event/UsersFromEvents.java
+++ src/main/java/com/github/princesslana/eriscasper/repository/event/UsersFromEvents.java
@@ -1,20 +1,29 @@
 package com.github.princesslana.eriscasper.repository.event;
 
+import com.github.princesslana.eriscasper.data.Snowflake;
 import com.github.princesslana.eriscasper.data.event.Event;
+import com.github.princesslana.eriscasper.data.event.GuildCreateEvent;
 import com.github.princesslana.eriscasper.data.event.ReadyEvent;
+import com.github.princesslana.eriscasper.data.event.ReadyEventData;
+import com.github.princesslana.eriscasper.data.resource.Guild;
 import com.github.princesslana.eriscasper.data.resource.User;
 import com.github.princesslana.eriscasper.repository.UserRepository;
 import io.reactivex.Observable;
 import io.reactivex.Single;
 import io.reactivex.observables.ConnectableObservable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UsersFromEvents implements UserRepository {
 
   private ConnectableObservable<User> self;
+  private Map<Snowflake, Guild> guildCache;
 
   public UsersFromEvents(Observable<Event> events) {
-    self = events.ofType(ReadyEvent.class).map(ReadyEvent::unwrap).map(d -> d.getUser()).replay(1);
-
+    self = events.ofType(ReadyEvent.class).map(ReadyEvent::unwrap).map(ReadyEventData::getUser).replay(1);
+    guildCache = new HashMap<>();
+    events.ofType(GuildCreateEvent.class).map(GuildCreateEvent::unwrap).subscribe(guild -> guildCache.putIfAbsent(guild.getId(), guild));
     self.connect();
   }
 
