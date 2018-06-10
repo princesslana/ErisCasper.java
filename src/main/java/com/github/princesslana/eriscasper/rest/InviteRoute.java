7f8b60f4b515745ce6efc5bbbef5530caf782c0f
CoreyShupe <coreyshupe101@gmail.com>
2018-06-09 20:02:01 -0400

    Invite resources. Addresses https://github.com/princesslana/ErisCasper.java/issues/127

---
+++ src/main/java/com/github/princesslana/eriscasper/rest/InviteRoute.java
@@ -0,0 +1,38 @@
+package com.github.princesslana.eriscasper.rest;
+
+import com.github.princesslana.eriscasper.data.resource.Invite;
+import java.util.Optional;
+
+public class InviteRoute {
+
+  private final String path;
+
+  private InviteRoute(String code) {
+    this.path = "/invites/" + code;
+  }
+
+  /**
+   * @see <a href="https://discordapp.com/developers/docs/resources/invite#get-invite">
+   *     https://discordapp.com/developers/docs/resources/invite#get-invite</a>
+   */
+  public Route<Void, Invite> get(Optional<Boolean> withCounts) {
+    return Route.get(
+        path + withCounts.map(bool -> ";with_counts=" + bool).orElse(""), Invite.class);
+  }
+
+  /**
+   * @see <a href="https://discordapp.com/developers/docs/resources/invite#delete-invite">
+   *     https://discordapp.com/developers/docs/resources/invite#delete-invite</a>
+   */
+  public Route<Void, Invite> delete() {
+    return Route.delete(path, Invite.class);
+  }
+
+  public static InviteRoute of(String code) {
+    return new InviteRoute(code);
+  }
+
+  public static InviteRoute of(Invite invite) {
+    return of(invite.getCode());
+  }
+}
