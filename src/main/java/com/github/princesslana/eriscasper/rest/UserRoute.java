2969ca43503ff41f6ea2a3b3dcb19da268189358
CoreyShupe <coreyshupe101@gmail.com>
2018-06-10 22:49:23 -0400

    User resource support. Addresses https://github.com/princesslana/ErisCasper.java/issues/128

---
+++ src/main/java/com/github/princesslana/eriscasper/rest/UserRoute.java
@@ -0,0 +1,101 @@
+package com.github.princesslana.eriscasper.rest;
+
+import com.github.princesslana.eriscasper.data.Snowflake;
+import com.github.princesslana.eriscasper.data.resource.Channel;
+import com.github.princesslana.eriscasper.data.resource.Connection;
+import com.github.princesslana.eriscasper.data.resource.Guild;
+import com.github.princesslana.eriscasper.data.resource.User;
+import com.github.princesslana.eriscasper.rest.user.CreateDMRequest;
+import com.github.princesslana.eriscasper.rest.user.CreateGroupDMRequest;
+import com.github.princesslana.eriscasper.rest.user.GetUserGuildsRequest;
+import com.github.princesslana.eriscasper.rest.user.ModifyUserRequest;
+import com.google.common.collect.ImmutableList;
+import java.util.Arrays;
+
+public final class UserRoute {
+
+  private UserRoute() {}
+
+  /**
+   * @see <a href="https://discordapp.com/developers/docs/resources/user#get-current-user">
+   *     https://discordapp.com/developers/docs/resources/user#get-current-user</a>
+   */
+  public static Route<Void, User> getMe() {
+    return Route.get(myPath(""), User.class);
+  }
+
+  /**
+   * @see <a href="https://discordapp.com/developers/docs/resources/user#get-user">
+   *     https://discordapp.com/developers/docs/resources/user#get-user</a>
+   */
+  public static Route<Void, User> getUser(Snowflake userId) {
+    return Route.get(path("/%s", userId.unwrap()), User.class);
+  }
+
+  /**
+   * @see <a href="https://discordapp.com/developers/docs/resources/user#modify-current-user">
+   *     https://discordapp.com/developers/docs/resources/user#modify-current-user</a>
+   */
+  public static Route<ModifyUserRequest, User> modifyMe() {
+    return Route.patch(myPath(""), ModifyUserRequest.class, User.class);
+  }
+
+  /**
+   * @see <a href="https://discordapp.com/developers/docs/resources/user#get-current-user-guilds">
+   *     https://discordapp.com/developers/docs/resources/user#get-current-user-guilds</a>
+   */
+  public static Route<GetUserGuildsRequest, ImmutableList<Guild>> getMyGuilds() {
+    return Route.get(
+        myPath("/guilds"),
+        Route.queryString(GetUserGuildsRequest::toQueryString),
+        Route.jsonArrayResponse(Guild.class));
+  }
+
+  /**
+   * @see <a href="https://discordapp.com/developers/docs/resources/user#leave-guild">
+   *     https://discordapp.com/developers/docs/resources/user#leave-guild</a>
+   */
+  public static Route<Void, Void> leaveGuild(Snowflake guildId) {
+    return Route.delete(myPath("/guilds/%s", guildId.unwrap()), Void.class);
+  }
+
+  /**
+   * @see <a href="https://discordapp.com/developers/docs/resources/user#get-user-dms">
+   *     https://discordapp.com/developers/docs/resources/user#get-user-dms</a>
+   */
+  public static Route<Void, ImmutableList<Channel>> getMyDMs() {
+    return Route.get(myPath("/channels"), Route.jsonArrayResponse(Channel.class));
+  }
+
+  /**
+   * @see <a href="https://discordapp.com/developers/docs/resources/user#create-dm">
+   *     https://discordapp.com/developers/docs/resources/user#create-dm</a>
+   */
+  public static Route<CreateDMRequest, Channel> createDM() {
+    return Route.post(myPath("/channels"), CreateDMRequest.class, Channel.class);
+  }
+
+  /**
+   * @see <a href="https://discordapp.com/developers/docs/resources/user#create-group-dm">
+   *     https://discordapp.com/developers/docs/resources/user#create-group-dm</a>
+   */
+  public static Route<CreateGroupDMRequest, Channel> createGroupDm() {
+    return Route.post(myPath("/channels"), CreateGroupDMRequest.class, Channel.class);
+  }
+
+  /**
+   * @see <a href="https://discordapp.com/developers/docs/resources/user#get-user-connections">
+   *     https://discordapp.com/developers/docs/resources/user#get-user-connections</a>
+   */
+  public static Route<Void, ImmutableList<Connection>> getMyConnections() {
+    return Route.get(myPath("/connections"), Route.jsonArrayResponse(Connection.class));
+  }
+
+  private static String path(String fmt, String... args) {
+    return "/users" + String.format(fmt, Arrays.asList(args).toArray());
+  }
+
+  private static String myPath(String fmt, String... args) {
+    return path("/@me") + String.format(fmt, Arrays.asList(args).toArray());
+  }
+}
---
+++ src/main/java/com/github/princesslana/eriscasper/rest/user/CreateDMRequest.java
@@ -0,0 +1,17 @@
+package com.github.princesslana.eriscasper.rest.user;
+
+import com.github.princesslana.eriscasper.data.Snowflake;
+import org.immutables.value.Value;
+
+/**
+ * @see <a href="https://discordapp.com/developers/docs/resources/user#create-dm-json-params">
+ *     https://discordapp.com/developers/docs/resources/user#create-dm-json-params</a>
+ */
+@Value.Immutable
+public interface CreateDMRequest {
+  Snowflake getRecipientId();
+
+  static CreateDMRequest with(Snowflake userId) {
+    return ImmutableCreateDMRequest.builder().recipientId(userId).build();
+  }
+}
