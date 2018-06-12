48ea8cf3b19c7478feca276b76d96551a23e5879
CoreyShupe <coreyshupe101@gmail.com>
2018-06-12 08:17:00 -0400

    Fixed json issue with `Identify`.

--- src/main/java/com/github/princesslana/eriscasper/gateway/commands/Identify.java
+++ src/main/java/com/github/princesslana/eriscasper/gateway/commands/Identify.java
@@ -1,5 +1,6 @@
 package com.github.princesslana.eriscasper.gateway.commands;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.github.princesslana.eriscasper.BotToken;
 import com.github.princesslana.eriscasper.gateway.commands.util.ConnectionProperties;
 import java.util.Optional;
@@ -10,6 +11,7 @@ import org.immutables.value.Value;
  *     https://discordapp.com/developers/docs/topics/gateway#identify</a>
  */
 // TODO: This structure is not complete
+@JsonDeserialize(as = ImmutableIdentify.class)
 @Value.Immutable
 public interface Identify {
   BotToken getToken();
