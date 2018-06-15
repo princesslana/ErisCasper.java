cc7076e7d3e182158ed78604aabec823047f8c72
CoreyShupe <coreyshupe101@gmail.com>
2018-06-15 01:56:22 -0400

    Styling.

--- src/test/java/com/github/princesslana/eriscasper/util/ShardTest.java
+++ src/test/java/com/github/princesslana/eriscasper/util/ShardTest.java
@@ -4,34 +4,37 @@ import com.github.princesslana.eriscasper.ErisCasperFatalException;
 import com.ufoscout.properlty.Properlty;
 import com.ufoscout.properlty.reader.ProgrammaticPropertiesReader;
 import com.ufoscout.properlty.reader.Properties;
-import java.util.Optional;
 import org.assertj.core.api.Assertions;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ShardTest {
 
   @Test
   public void fromConfig_whenBothValuesPresent_shouldSucceed() {
-    Assert.assertEquals(
-        Shard.fromConfig(generateProperlty(Optional.of(2), Optional.of(3))).get().unwrap(),
-        new Integer[] {2, 3});
+    Assertions.assertThat(
+            Shard.fromConfig(
+                properlty(Properties.add("ec.shard.id", "2").add("ec.shard.total", "3"))))
+        .map(Shard::unwrap)
+        .hasValue(new Integer[] {2, 3});
   }
 
   @Test
   public void fromConfig_whenBothValuesEmpty_shouldBeEmpty() {
-    Assert.assertFalse(
-        Shard.fromConfig(generateProperlty(Optional.empty(), Optional.empty())).isPresent());
+    Assertions.assertThat(Shard.fromConfig(properlty())).isEmpty();
   }
 
   @Test
-  public void fromConfig_whenOneValuePresent_shouldThrowException() {
+  public void fromConfig_whenOnlyIdPresent_shouldThrowException() {
     Assertions.assertThatThrownBy(
-            () -> Shard.fromConfig(generateProperlty(Optional.empty(), Optional.of(3))))
+            () -> Shard.fromConfig(properlty(Properties.add("ec.shard.id", "2"))))
         .isInstanceOf(ErisCasperFatalException.class)
         .hasMessageContaining("resolve");
+  }
+
+  @Test
+  public void fromConfig_whenOnlyTotalPresent_shouldThrowException() {
     Assertions.assertThatThrownBy(
-            () -> Shard.fromConfig(generateProperlty(Optional.of(2), Optional.empty())))
+            () -> Shard.fromConfig(properlty(Properties.add("ec.shard.total", "3"))))
         .isInstanceOf(ErisCasperFatalException.class)
         .hasMessageContaining("resolve");
   }
@@ -39,15 +42,18 @@ public class ShardTest {
   @Test
   public void fromConfig_whenShardHigherThanTotal_shouldThrowError() {
     Assertions.assertThatThrownBy(
-            () -> Shard.fromConfig(generateProperlty(Optional.of(3), Optional.of(2))))
+            () ->
+                Shard.fromConfig(
+                    properlty(Properties.add("ec.shard.id", "3").add("ec.shard.total", "2"))))
         .isInstanceOf(ErisCasperFatalException.class)
         .hasMessageContaining("create");
   }
 
-  private Properlty generateProperlty(Optional<Integer> shardNumber, Optional<Integer> shardTotal) {
-    ProgrammaticPropertiesReader reader = Properties.add("def", "def");
-    shardNumber.ifPresent(shard -> reader.add("ec.shard.id", String.valueOf(shard)));
-    shardTotal.ifPresent(total -> reader.add("ec.shard.total", String.valueOf(total)));
+  private Properlty properlty() {
+    return Properlty.builder().build();
+  }
+
+  private Properlty properlty(ProgrammaticPropertiesReader reader) {
     return Properlty.builder().add(reader).build();
   }
 }
