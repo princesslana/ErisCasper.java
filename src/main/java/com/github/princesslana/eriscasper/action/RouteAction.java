a9b1c3a9d9199eed3c43ff396f2615b328c9055e
Corey Shupe <coreyshupe101@gmail.com>
2018-07-05 19:17:17 -0400

    Changed `RouteAction`'s data to `Nullable`. This better fits what we're looking for in a `RouteAction`. The data is not optional, it can however be null.

--- src/main/java/com/github/princesslana/eriscasper/action/RouteAction.java
+++ src/main/java/com/github/princesslana/eriscasper/action/RouteAction.java
@@ -1,8 +1,8 @@
 package com.github.princesslana.eriscasper.action;
 
+import com.github.princesslana.eriscasper.data.util.Nullable;
 import com.github.princesslana.eriscasper.rest.Route;
 import io.reactivex.Completable;
-import java.util.Optional;
 import org.immutables.value.Value;
 
 @Value.Immutable
@@ -10,11 +10,11 @@ public abstract class RouteAction<I, O> implements Action {
 
   public abstract Route<I, O> getRoute();
 
-  public abstract Optional<I> getData();
+  public abstract Nullable<I> getData();
 
   @Override
   public Completable apply(ActionContext context) {
-    return context.getRoutes().execute(getRoute(), getData().orElse(null)).toCompletable();
+    return context.getRoutes().execute(getRoute(), getData().orNull()).toCompletable();
   }
 
   static <O> RouteAction<Void, O> of(Route<Void, O> route) {
@@ -22,10 +22,13 @@ public abstract class RouteAction<I, O> implements Action {
   }
 
   static <I, O> RouteAction<I, O> of(Route<I, O> route, I data) {
-    return ImmutableRouteAction.<I, O>builder().route(route).data(data).build();
+    return ImmutableRouteAction.<I, O>builder()
+        .route(route)
+        .data(Nullable.ofNullable(data))
+        .build();
   }
 
-  static <I, O> RouteAction<I, O> of(Route<I, O> route, Optional<I> data) {
+  static <I, O> RouteAction<I, O> of(Route<I, O> route, Nullable<I> data) {
     return ImmutableRouteAction.<I, O>builder().route(route).data(data).build();
   }
 }
